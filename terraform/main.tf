5ba352d661f0d09f46f37808b6f6b16fc94485db
Alexandr Poddubnyy <a.n.poddubnyy@gmail.com>
2023-08-29 19:53:22 +0300

    HW6 Terraform Base

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,10 +2,9 @@ terraform {
   required_providers {
     yandex = {
       source  = "yandex-cloud/yandex"
-#      version = "0.95.0"
+      version = "0.109.0"
     }
   }
-  required_version = ">= 0.13"
 }
 
 provider "yandex" {
