1344d94e5b09ec4131b17a5ffb921b3aab6f093c
Alexandr Poddubnyy <a.n.poddubnyy@gmail.com>
2023-08-29 19:12:39 +0300

    HW6 Terraform Base

---
+++ terraform/files/deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+APP_DIR=${1:-$HOME}
+#sleep 10
+while [ -n "$(pgrep apt-get)" ]; do sleep 1; i=$((i+1)); echo $i ; done
+sudo apt-get install -y git --allow-unauthenticated
+git clone -b monolith https://github.com/express42/reddit.git $APP_DIR/reddit
+cd $APP_DIR/reddit
+bundle install
+sudo mv /tmp/puma.service /etc/systemd/system/puma.service
+sudo systemctl start puma
+sudo systemctl enable puma
