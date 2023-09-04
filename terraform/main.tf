1852be9ebb3064387ce0c2626b7718fc2ea7b63c
Alexandr Poddubnyy <a.n.poddubnyy@gmail.com>
2023-09-04 22:06:59 +0300

    HW6 Terraform 1. test fix

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,13 +1,3 @@
-terraform {
-  required_providers {
-    yandex = {
-      source  = "yandex-cloud/yandex"
-      version = "~>0.95"
-    }
-  }
-  required_version = "~>1.5.1"
-}
-
 
 provider "yandex" {
   service_account_key_file = var.service_account_key_file
