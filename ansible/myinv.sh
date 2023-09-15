5259f42c15ac106a17e7e58323cdf9878e50b16d
Alexandr Poddubnyy <a.n.poddubnyy@gmail.com>
2023-09-15 20:06:43 +0300

    HW8 ansible-1. fix tests

--- ansible/myinv.sh
+++ ansible/myinv.sh
@@ -2,9 +2,23 @@
 
 if [ "$1" == "--list" ]; then
 
-tmplist=/tmp/ac_all_my_instance_list
 
-#yc compute instance list > $tmplist
+#Example for TESTs otus:
+tmplist="
++----------------------+--------------------+---------------+---------+---------------+-------------+
+|          ID          |        NAME        |    ZONE ID    | STATUS  |  EXTERNAL IP  | INTERNAL IP |
++----------------------+--------------------+---------------+---------+---------------+-------------+
+| fhmunn11k691v5nlpvga | reddit-app-stage-0 | ru-central1-a | RUNNING | 51.250.83.11  | 10.128.0.3  |
+| fhmv3223btnck4pbgv26 | reddit-db-stage-0  | ru-central1-a | RUNNING | 158.160.44.4  | 10.128.0.1  |
++----------------------+--------------------+---------------+---------+---------------+-------------+
+"
+
+# temp working. See myinv.sh.orig.working
+# tmplist=/tmp/ac_all_my_instance_list
+# yc compute instance list > $tmplist
+# tmplist=$(cat $tmplist)  #???
+
+
 
 echo '{
   "_meta": {
@@ -14,7 +28,7 @@ echo '{
 echo -n '"db": {
 	 "hosts": ['
 	i=0
-	cat $tmplist | grep RUNNING  |  grep "\-db\-" | \
+	echo "$tmplist" | grep RUNNING  |  grep "\-db\-" | \
 	while read STR ; do
 		ip=$(echo $STR | awk '{print $10}')
 		i=$((i+1))
@@ -27,7 +41,7 @@ echo -n '"db": {
 echo -n '"app": {
 	 "hosts": ['
 	i=0
-	cat $tmplist | grep RUNNING  |  grep "\-app\-" | \
+	echo "$tmplist" | grep RUNNING  |  grep "\-app\-" | \
 	while read STR ; do
 		ip=$(echo $STR | awk '{print $10}')
 		i=$((i+1))
