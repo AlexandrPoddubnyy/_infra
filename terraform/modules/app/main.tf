acebe42e60a8abf6b2df68de88dddc00b5bc3700
Alexandr Poddubnyy <a.n.poddubnyy@gmail.com>
2023-09-18 16:12:09 +0300

    HW9 ansible-2 . remark * terraform2 deploy

--- terraform/modules/app/main.tf
+++ terraform/modules/app/main.tf
@@ -31,21 +31,22 @@ resource "yandex_compute_instance" "app" {
     ssh-keys = "ubuntu:${file(var.public_key_path)}"
   }
 
-  connection {
-    type        = "ssh"
-    host        = self.network_interface.0.nat_ip_address
-    user        = "ubuntu"
-    agent       = false
-    private_key = file(var.private_key_path)
-  }
-
-  provisioner "file" {
-    content     = templatefile("${path.module}/puma.service.tpl", { ip_address_db = "${var.db_ip}" })
-    destination = "/tmp/puma.service"
-  }
-
-  provisioner "remote-exec" {
-    script = "${path.module}/deploy_app.sh"
-  }
+# (*) off in Hw9 ansivle 2
+#  connection {
+#    type        = "ssh"
+#    host        = self.network_interface.0.nat_ip_address
+#    user        = "ubuntu"
+#    agent       = false
+#    private_key = file(var.private_key_path)
+#  }
+#
+#  provisioner "file" {
+#    content     = templatefile("${path.module}/puma.service.tpl", { ip_address_db = "${var.db_ip}" })
+#    destination = "/tmp/puma.service"
+#  }
+#
+#  provisioner "remote-exec" {
+#    script = "${path.module}/deploy_app.sh"
+#  }
 
 }
