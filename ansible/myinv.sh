d894f23d8fa7a7a3bb08169f1aa8ed11a70dd1ac
Alexandr Poddubnyy <a.n.poddubnyy@gmail.com>
2023-09-18 21:01:25 +0300

    HW9 ansible-2 . update outputs in terraform (internal ip)

--- ansible/myinv.sh
+++ ansible/myinv.sh
@@ -2,23 +2,9 @@
 
 if [ "$1" == "--list" ]; then
 
+tmplist=/tmp/ac_all_my_instance_list
 
-#Example for TESTs otus:
-tmplist="
-+----------------------+--------------------+---------------+---------+---------------+-------------+
-|          ID          |        NAME        |    ZONE ID    | STATUS  |  EXTERNAL IP  | INTERNAL IP |
-+----------------------+--------------------+---------------+---------+---------------+-------------+
-| fhmunn11k691v5nlpvga | reddit-app-stage-0 | ru-central1-a | RUNNING | 51.250.83.11  | 10.128.0.3  |
-| fhmv3223btnck4pbgv26 | reddit-db-stage-0  | ru-central1-a | RUNNING | 158.160.44.4  | 10.128.0.1  |
-+----------------------+--------------------+---------------+---------+---------------+-------------+
-"
-
-# temp working. See myinv.sh.orig.working
-# tmplist=/tmp/ac_all_my_instance_list
-# yc compute instance list > $tmplist
-# tmplist=$(cat $tmplist)  #???
-
-
+yc compute instance list > $tmplist
 
 echo '{
   "_meta": {
@@ -28,7 +14,7 @@ echo '{
 echo -n '"db": {
 	 "hosts": ['
 	i=0
-	echo "$tmplist" | grep RUNNING  |  grep "\-db\-" | \
+	cat $tmplist | grep RUNNING  |  grep "\-db\-" | \
 	while read STR ; do
 		ip=$(echo $STR | awk '{print $10}')
 		i=$((i+1))
@@ -41,7 +27,7 @@ echo -n '"db": {
 echo -n '"app": {
 	 "hosts": ['
 	i=0
-	echo "$tmplist" | grep RUNNING  |  grep "\-app\-" | \
+	cat $tmplist | grep RUNNING  |  grep "\-app\-" | \
 	while read STR ; do
 		ip=$(echo $STR | awk '{print $10}')
 		i=$((i+1))
